f9dcffb3717f219adb3e72b168fe4b16ae519b4f
Mustafa Tahir <124084701+Mufasa8066@users.noreply.github.com>
2024-07-30 22:34:08 -0500

    submitted

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+export default function Home() {
+  return(
+    <Box 
+      width="100vw" 
+      height="100vh"
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    > Hello World
+    </Box>
+  )
+}
