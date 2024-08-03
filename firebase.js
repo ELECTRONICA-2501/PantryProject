4612429eedeaf466e0b2f716079c0cf6523fddd6
Mustafa Tahir <124084701+Mufasa8066@users.noreply.github.com>
2024-08-03 15:45:19 -0500

    Newest changes

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
-import { initializeApp } from 'firebase/app'
-import { getAnalytics } from 'firebase/analytics'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from 'firebase/app';
+//import { getAnalytics } from 'firebase/analytics';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,8 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
-const firestore = getFirestore(app)
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
 
 export {firestore}
