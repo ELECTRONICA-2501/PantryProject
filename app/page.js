f62a1795a73862212c2430ae60ba11b8a001f278
Mustafa Tahir <124084701+Mufasa8066@users.noreply.github.com>
2024-08-03 11:00:16 -0500

    New Commit

--- app/page.js
+++ app/page.js
@@ -1,14 +1,45 @@
-import { Box } from "@mui/material";
+'use client'
+import {Box, Typography} from '@mui/material'
+import Image from 'next/image'
+import {useState, useEffect} from 'react'
+import {firestore} from '@/firebase'
+
+
+
 
 export default function Home() {
-  return(
-    <Box 
-      width="100vw" 
-      height="100vh"
-      display={"flex"}
-      justifyContent={"center"}
-      alignItems={"center"}
-    > Hello World
-    </Box>
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState (false)
+  const [itemName, setItemName] = useState('')
+
+  const updateInventory = async() => {
+    const snapshot = query(collections (firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList=[]
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name:doc.id,
+        ...doc.data(),
+      })
+    })
+    setInventory(inventoryList)
+  }
+
+useEffect(() => {
+  updateInventory()
+}, [])
+
+
+  return(  
+    <Box>
+      <Typography variant="h1">Inventory Management</Typography>
+      {
+        inventory.forEach((item)=>{
+          return<>
+            {item.name}
+            {item.count}
+            </>
+        })}
+    </Box>     
   )
 }
